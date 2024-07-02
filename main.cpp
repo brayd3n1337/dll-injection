5898f330a4879766414ad24b10fe2195476d9e66
brayden <111473970+Creida1337@users.noreply.github.com>
2024-07-02 04:19:15 -0400

    fixed the injection being really weird and not working sometimes (i recoded it :sob:)

--- main.cpp
+++ main.cpp
@@ -40,107 +40,120 @@ DWORD GetProcessId(const char* processName)
     return 0;
 }
 
+
 int main()
 {
-    // C STYLE STRINGS ARENT THEY AMAZING?
-    const char* dllPath = R"(C:\Users\creid\CLionProjects\mapping-injection\testttt.dll)";
-    const char* processName = "java.exe";
+    const char* processName = "javaw.exe";
 
-    // Get process ID of the target process
-    DWORD pid = 11180;
+    // replace with your own
+    const char* dllPath = R"(C:\Users\creid\OneDrive\Documents\GitHub\plaguemc\x64\Release\plague-nextgen.dll)";
 
-    if (pid == 0)
+    const DWORD processId = GetProcessId(processName);
+
+    if (processId == 0)
     {
-        ERROR("failed 2 get PID for %s. process not found or access denied. Error: %lu", processName, GetLastError());
-        ERROR("failed 2 create handle opening da process (invalid pid?). %lu", GetLastError());
+        ERROR("failed 2 get process id of %s", processName);
         return 1;
     }
 
-    INFO("Found process %s with PID %lu", processName, pid);
+    // create a handle to the process
+    // this handle is responsible for all operations on the process
+    HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
 
-    // open the target process
-    HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
-
+    // check if the handle is valid
     if (processHandle == nullptr)
     {
-        ERROR("failed to open process. %lu", GetLastError());
+        ERROR("failed 2 open process %lu. error: %lu", processId, GetLastError());
         return 1;
     }
 
-    INFO("successfully opened process with handle 0x%p", processHandle);
+    // allocate the memory into the target process
+    // we use the length of the dll path + 1 to include the null terminator
+    // VirtualAllocEx allocates memory into a target process, hence why we aren't using malloc or new (obviously)
+    LPVOID dllPathAddress = VirtualAllocEx(processHandle, nullptr, strlen(dllPath) + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 
-    // allocate memory in the target process for the dll path
-    LPVOID baseAddr = VirtualAllocEx(processHandle, nullptr, strlen(dllPath) + 1, MEM_COMMIT, PAGE_READWRITE);
-
-    if (baseAddr == nullptr)
+    // check if its null
+    if (dllPathAddress == nullptr)
     {
-        ERROR("failed to allocate memory into the target process. %lu", GetLastError());
+        ERROR("failed 2 allocate memory in process %lu. error: %lu", processId, GetLastError());
         CloseHandle(processHandle);
         return 1;
     }
 
-    // write the DLL path into the allocated memory in the target process
-    if (!WriteProcessMemory(processHandle, baseAddr, dllPath, strlen(dllPath) + 1, nullptr))
+
+    if (!WriteProcessMemory(processHandle, dllPathAddress, dllPath, strlen(dllPath) + 1, nullptr))
     {
-        ERROR("failed to write DLL path to target process memory. %lu", GetLastError());
-        VirtualFreeEx(processHandle, baseAddr, 0, MEM_RELEASE);
+        ERROR("failed 2 write memory in process %lu. Error: %lu", processId, GetLastError());
+
+        // cleanup
+        VirtualFreeEx(processHandle, dllPathAddress, 0, MEM_RELEASE);
         CloseHandle(processHandle);
         return 1;
     }
 
-    INFO("wrote DLL path %s to target process memory", dllPath);
+    // we cant do anything if we dont have this handle
+    // the kernel 32 handle is responsible for loading the dll into the process
+    // each windows process has a kernel32.dll loaded into it
+    HMODULE kernel32 = GetModuleHandleA("kernel32.dll");
 
-    // get the handle to kernel32.dll
-    HMODULE kernelHandle = GetModuleHandleA("kernel32.dll");
-
-    // check if null
-    if (kernelHandle == nullptr)
+    if (kernel32 == nullptr)
     {
-        ERROR("Failed to get handle to kernel32.dll. Error: %lu", GetLastError());
-        VirtualFreeEx(processHandle, baseAddr, 0, MEM_RELEASE);
+        ERROR("failed 2 get handle to kernel32.dll. Error: %lu", GetLastError());
+
+        // cleanup
+        VirtualFreeEx(processHandle, dllPathAddress, 0, MEM_RELEASE);
         CloseHandle(processHandle);
         return 1;
     }
 
-    INFO("got handle to kernel32.dll at address yayyyy :3 0x%p", kernelHandle);
+    // get the address of LoadLibraryA
+    FARPROC LLAAddress = GetProcAddress(kernel32, "LoadLibraryA");
 
-    // get address of LoadLibraryA function from the kernel handle
-    // GetProcAddress function retrieves the address of an exported function or variable from the specified DLL.
-    auto kernelLLAAddress = (LPTHREAD_START_ROUTINE) GetProcAddress(kernelHandle, "LoadLibraryA");
-
-    if (kernelLLAAddress == nullptr)
+    // if we cant get the address of LoadLibraryA, we cant load the dll
+    if (LLAAddress == nullptr)
     {
-        ERROR("failed 2 get address of LLA function. Error: %lu", GetLastError());
-        VirtualFreeEx(processHandle, baseAddr, 0, MEM_RELEASE);
+        ERROR("failed 2 get LLA address. Error: %lu", GetLastError());
+        VirtualFreeEx(processHandle, dllPathAddress, 0, MEM_RELEASE);
         CloseHandle(processHandle);
         return 1;
     }
 
-    INFO("got address of LLA function at 0x%p", kernelLLAAddress);
+    // create a remote thread in the target process
+    // this thread is responsible for actually loading it
+    HANDLE remoteThreadHandle = CreateRemoteThread(processHandle, nullptr, 0, (LPTHREAD_START_ROUTINE)LLAAddress, dllPathAddress, 0, nullptr);
 
-    // create remote thread in the target process to load the DLL
-    // the create remote thread function creates a thread in the address space of another process
-    HANDLE hRemoteThread = CreateRemoteThread(processHandle, nullptr, 0, kernelLLAAddress, baseAddr, 0, nullptr);
+    INFO("creating remote thread handle...");
 
-    if (hRemoteThread == nullptr)
+    if (remoteThreadHandle == nullptr)
     {
-        ERROR("failed to create remote thread in target process. Error: %lu", GetLastError());
-        VirtualFreeEx(processHandle, baseAddr, 0, MEM_RELEASE);
+        ERROR("failed 2 create remote thread in process %lu. error: %lu", processId, GetLastError());
+
+        // cleanup
+
+        VirtualFreeEx(processHandle, dllPathAddress, 0, MEM_RELEASE);
         CloseHandle(processHandle);
         return 1;
     }
 
-    INFO("created da remote thread in target process. thread handle: 0x%p", hRemoteThread);
+    INFO("Successfully created remote thread handle!");
 
-    // Wait for the remote thread to finish
-    WaitForSingleObject(hRemoteThread, INFINITE);
 
-    INFO("cleaning up ^_^ yipeeeee");
+    INFO("we are so fucking gucci it injected ;3 %s", processName);
 
-    // Clean up
-    VirtualFreeEx(processHandle, baseAddr, 0, MEM_RELEASE);
-    CloseHandle(hRemoteThread);
+
+    INFO("Waiting for the thread to finish...");
+    WaitForSingleObject(remoteThreadHandle, INFINITE);
+
+    INFO("Thread finished!");
+
+    INFO("Cleaning up...");
+
+
+    VirtualFreeEx(processHandle, dllPathAddress, 0, MEM_RELEASE);
+    CloseHandle(remoteThreadHandle);
     CloseHandle(processHandle);
+
+
 
     return 0;
 }
